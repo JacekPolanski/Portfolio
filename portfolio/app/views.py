412bbae60404a4aadcd052d011029be8ee23dcf0
Jacek Polański <kontakt@jpolanski.pl>
2015-08-27 22:18:04 +0200

    [app] index template - banner section added

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
-from .models import Contact
+from .models import Contact, AboutMe
 
 
 class IndexView(View):
     def get(self, request):
         contact = get_object_or_404(Contact)
+        about_me = get_object_or_404(AboutMe)
 
         template_data = {
-            'contact': contact
+            'contact': contact,
+            'about_me': about_me,
         }
 
         return render(request, 'app/index.html', template_data)
