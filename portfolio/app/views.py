9353b65bed313002efa9566878ed1bcdf5f88453
Jacek Polański <kontakt@jpolanski.pl>
2015-08-27 22:41:51 +0200

    [app] index template - skills section added

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import View
-from .models import Contact, AboutMe
+from .models import Contact, AboutMe, Skill
 
 
 class IndexView(View):
     def get(self, request):
         contact = get_object_or_404(Contact)
         about_me = get_object_or_404(AboutMe)
+        skill = get_list_or_404(Skill)
 
         template_data = {
             'contact': contact,
             'about_me': about_me,
+            'skills': skill,
         }
+
+        print(skill)
 
         return render(request, 'app/index.html', template_data)
