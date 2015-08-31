008ebc77d51226280807229ad7695390a8991818
Jacek Polański <kontakt@jpolanski.pl>
2015-08-31 20:10:44 +0200

    [app] MEDIA_URL added + app url changed to /app

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'', include('app.urls', namespace="app")),
+    url(r'^app$', include('app.urls', namespace="app")),
     url(r'^admin/', include(admin.site.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
