ee16d75d27655a0169f38833f85af212fe2b4de8
Jacek Polański <kontakt@jpolanski.pl>
2015-08-31 21:11:06 +0200

    [app] index template - projects section added

--- portfolio/app/views.py
+++ portfolio/app/views.py
@@ -1,20 +1,22 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import View
-from .models import Contact, AboutMe, Skill
+from .models import Contact, AboutMe, Skill, Project
 
 
 class IndexView(View):
     def get(self, request):
         contact = get_object_or_404(Contact)
         about_me = get_object_or_404(AboutMe)
-        skill = get_list_or_404(Skill)
+        skills = get_list_or_404(Skill)
+        not_owned_projects = get_list_or_404(Project.objects.filter(is_owner=False))
+        owned_projects = get_list_or_404(Project.objects.filter(is_owner=True))
 
         template_data = {
             'contact': contact,
             'about_me': about_me,
-            'skills': skill,
+            'skills': skills,
+            'owned_projects': owned_projects,
+            'not_owned_projects': not_owned_projects
         }
-
-        print(skill)
 
         return render(request, 'app/index.html', template_data)
