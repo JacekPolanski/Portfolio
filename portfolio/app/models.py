91d51f7f99a3fd5db90b4ee804007cdcc92cf98c
Jacek Polański <kontakt@jpolanski.pl>
2016-01-30 19:34:39 +0100

    [app] Models - Added to string methods for better admin view

--- portfolio/app/models.py
+++ portfolio/app/models.py
@@ -8,6 +8,9 @@ class AboutMe(models.Model):
     date_added = models.DateTimeField()
     description = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name_plural = 'About me'
 
@@ -20,11 +23,17 @@ class Project(models.Model):
     url = models.URLField()
     thumb = models.ImageField(upload_to='projects/')
 
+    def __str__(self):
+        return self.title
+
 
 class Skill(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=500)
     date_added = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
 
 
 class Contact(models.Model):
@@ -37,10 +46,16 @@ class Contact(models.Model):
     postal_code = models.CharField(max_length=8)
     profession = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
+
 
 class Community(models.Model):
     title = models.CharField(max_length=100)
     url = models.URLField()
+
+    def __str__(self):
+        return self.title
 
     class Meta:
         verbose_name_plural = 'Communities'
